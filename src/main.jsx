aa7cc4ee023f7cdb6a9964479b5b3c5db3f9a57c
Albertooantunezz <125560597+Albertooantunezz@users.noreply.github.com>
2025-05-29 21:36:50 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import './index-v2.css;
-import './App-v2.css'; 
+import './App.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
