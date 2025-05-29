e40c3f10a872ceea6619f1f9254fd5ae26b2b86e
Albertooantunezz <125560597+Albertooantunezz@users.noreply.github.com>
2025-05-29 21:47:35 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   build: {
+    cssCodeSplit: true, // asegura que el CSS salga como archivo separado
+  },
   plugins: [react()],
 })
